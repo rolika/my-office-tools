c53b3b839e33ae31e1dc4e211ffe2cf3e3ec97c8
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-04-11 06:55:16 +0200

    wip

---
+++ test/test_copy_folder.py
@@ -0,0 +1,35 @@
+import pytest
+import pathlib
+import shutil
+from code import create_new_project_folder
+
+
+class TestCopyFolder:
+
+    def test_copy_folder(self):
+        """Test the copy mechanism of an existing sample folder"""
+        # Create a new project folder
+        project_folder = create_new_project_folder()
+        # Copy the sample folder
+        shutil.copytree(
+            str(pathlib.Path(__file__).parent.absolute()) + "/sample_folder",
+            str(project_folder) + "/sample_folder")
+        # Check if the folder was copied
+        assert project_folder.exists()
+        assert project_folder.is_dir()
+        # Check if the files were copied
+        assert project_folder.joinpath("sample_folder").exists()
+        assert project_folder.joinpath("sample_folder").is_dir()
+        assert project_folder.joinpath("sample_folder").joinpath("file1.txt").exists()
+        assert project_folder.joinpath("sample_folder").joinpath("file1.txt").is_file()
+        assert project_folder.joinpath("sample_folder").joinpath("file2.txt").exists()
+        assert project_folder.joinpath("sample_folder").joinpath("file2.txt").is_file()
+        assert project_folder.joinpath("sample_folder").joinpath("file3.txt").exists()
+        assert project_folder.joinpath("sample_folder").joinpath("file3.txt").is_file()
+        assert project_folder.joinpath("sample_folder").joinpath("file4.txt").exists()
+        assert project_folder.joinpath("sample_folder").joinpath("file4.txt").is_file()
+        assert project_folder.joinpath("sample_folder").joinpath("file5.txt").exists()
+        assert project_folder.joinpath("sample_folder").joinpath("file5.txt").is_file()
+        assert project_folder.joinpath("sample_folder").joinpath("file6.txt").exists()
+        assert project_folder.joinpath("sample_folder").joinpath("file6.txt").is_file()
+        assert project_folder.joinpath("sample_folder").joinpath("file7.txt").exists()
