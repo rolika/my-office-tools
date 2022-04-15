fef41e6d3e56ea1c996445b1b52b529d0ede2b8e
Weisz Roland <weisz.roland@pohlen-dach.hu>
2022-04-15 18:22:33 +0200

    use config files

--- code/cnpf.py
+++ code/cnpf.py
@@ -3,20 +3,33 @@ import shutil
 import configparser
 
 
+DEFAULT_CONFIG_FILE = "data/config.ini"
+
+
 def create_new_project_folder(**kwargs):
     """Create a new project folder"""
-    src = kwargs.pop("src", None)
-    dst = kwargs.pop("dst", None)
+
+    # parse the config file - default values
+    cfg = kwargs.pop("cfg", DEFAULT_CONFIG_FILE)
+    config = configparser.ConfigParser()
+    config.read(cfg)  # this is empty if the config file does not exist
+    cfg_src = config.get("path", "src", fallback=None)
+    cfg_dst = config.get("path", "dst", fallback=None)
+    
+    # parse the kwargs and overwrite default values if necessary
+    src = kwargs.pop("src", cfg_src)
+    dst = kwargs.pop("dst", cfg_dst)
 
     if not src:
-        raise ValueError("src is required")
+        raise ValueError("source folder is required")
     if not dst:
-        raise ValueError("dst is required")
+        raise ValueError("destination folder is required")
 
     src = pathlib.Path(src)
 
     if src.exists() and src.is_dir():
-        name = kwargs.pop("name", src.name)
+        cfg_name = config.get("path", "name", fallback=src.name)
+        name = kwargs.pop("name", cfg_name)
 
         dst = pathlib.Path(dst)
         if dst.exists() and dst.is_dir():
